81458b4aec87ddbabd33f87cfdcfbfae798bb6f4
JM8N96 <jelquist96@gmail.com>
2026-09-15 15:10:40 -0600

    Initial INFO 2420 repository

---
+++ practice.sql
@@ -0,0 +1,13 @@
+
+/** Grocry list:
+Bananas (4)
+Peanut BUtter (1)
+Dark Chocolate Bars (2)
+**/
+
+CREATE TABLE Groceries(id INTEGER PRIMARY KEY, name TEXT, quantity INT, aisle Integer)
+
+INSERT INTO Groceries VALUES (1, 'Bananas', 4, 7);
+INSERT INTO Groceries VALUES (2, 'Peanut Butter', 1), 2;
+INSERT INTO Groceries VALUES (3, 'Dark Chocolate Bars', 2, 2);
+select * from Groceries;
